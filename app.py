ea3f629587f603ef9c12464cc3f8c021f82c0c0e
ssinchanasm-dev <ssinchanasm@gmail.com>
2026-08-03 11:08:22 +0530

    Refine age input and voting eligibility messages
    Updated age input prompt and eligibility messages for clarity.

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
-
 st.title("checking the person eligible for vote or not")
-
-age=st.number_input("enter ur age")
- if st.button("submit"):
-   if age>=18:
-     st.success("you are eligible to vote")
-   else:
-     st.success("you are not eligible for vote")
-              
-
+age=st.number_input("enter your age...")
+if st.button("submit"):
+ if age>=18:
+   st.success("you are eligible to vote..")
+else:
+  st.success("you are not eligible to vote..")
